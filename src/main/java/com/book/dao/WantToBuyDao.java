b846396607494f5ec340b836d2709f0c006e7615
lixuy <lxy>
2016-05-29 14:23:21 +0800

    修改bug

---
+++ src/main/java/com/book/dao/WantToBuyDao.java
@@ -0,0 +1,10 @@
+package com.book.dao;
+
+import com.book.entity.WantToBuy;
+
+/**
+ * Created by admin on 2016/4/25.
+ */
+public interface WantToBuyDao extends  GenericDao<WantToBuy,String> {
+
+}
